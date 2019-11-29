7b82afc834c11bb8f5138d4c78937f35ecad6bb8
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2019-11-29 15:31:07 +0100

    Acciones y Reducer basico

--- 01-redux-basic/app-1.ts
+++ 01-redux-basic/app-1.ts
@@ -1,1 +1,27 @@
-console.log('Hola Mundo');
+// ACTIONs
+interface Action {
+    type: string;
+    payload?: any;
+}
+
+// const incrementadorAction: Action = {
+//     type: 'INCREMENTAR'
+// };
+
+
+// REDUCERs
+function reducer(state = 10, action: Action) {
+    if(action.type == 'INCREMENTAR') {
+        return state += 1;
+    }
+
+    return state;
+}
+
+// Usar el reducer
+
+const incrementadorAction: Action = {
+    type: 'INCREMENTAR'
+};
+
+console.log(reducer(10, incrementadorAction));
