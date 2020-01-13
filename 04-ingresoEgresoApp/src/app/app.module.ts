f8a6f5bb55783092f10af8364fc48d78f950c6ee
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2020-01-13 13:18:10 +0100

    64 - Inicio del proyecto IngresoEgresoApp

---
+++ 04-ingresoEgresoApp/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { IngresoEgresoComponent } from './components/ingreso-egreso/ingreso-egreso.component';
+import { EstadisticaComponent } from './components/ingreso-egreso/estadistica/estadistica.component';
+import { DetalleComponent } from './components/ingreso-egreso/detalle/detalle.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    IngresoEgresoComponent,
+    EstadisticaComponent,
+    DetalleComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
